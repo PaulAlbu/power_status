2f62f53c17a79ab115840536adf11f544d9ccd41
Paul Albu <paul.i.albu@gmail.com>
2025-12-14 02:31:40 +0200

    added TODOs

--- src/main/java/com/power_status/power_status/Application.java
+++ src/main/java/com/power_status/power_status/Application.java
@@ -9,5 +9,18 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+//TODO
+//    1. Schimb clasele in lb engleza
+//    2 Sa vad daca nu pot sa mut logica din metoda getPowerOut() -> in Bean din clasa WebClientConfig
+//    3. Sa fac un client pt Intreruperi programate -> care sa imi aduca datele
+//    4. Gasit metoda prin care sa iterez peste intrerupreile accidentale (care e diferita de cele programate -> si ca obiect, dar si ca nu sunt anuntate inainte ca timp)
+//    5. Gasit cum imi dau seama daca adresa nu e exacta? Daca utilizatorul nu introduce aceeasi adresa ca si in datele mele?
+//    5.1 Aici la adresa pot fi mai multe adrese in fieldul adresa, dar o singura ora de start
+//    6. Gasit solutie ca si callul sa se faca o data la o ora
+//    7. Gasit aplicatie de trimis mail
+//    8. Gasit varianta aplicatie de refresh o data la ora
+//    9. Gasit varianta intreruperi accidentale -> daca s-au trimis pt acelea la utilizatorul X, sa nu se mai trimita
+//    10.Gasit varianta intreruperi programate -> mailul sa nu se trimita la fiecare ora ci o data la 3-4 zile si cu o zi inainte
+
 
 }
